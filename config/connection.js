d4bace164a816f7c2e88dfe187fd5c23b9177292
gokul3710 <gokulkrishnawaryar@gmail.com>
2023-02-03 12:30:44 +0530

    script code changed commit

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state = {
 }
 
 module.exports.connect = (done)=>{
-    const url = 'mongodb://localhost:27017'
+    const url = 'mongodb://127.0.0.1:27017'
     const dbname = 'cara'
 
     mongoClient.connect(url,(err,data)=>{
